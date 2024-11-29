2b33c14e637de5cdc60a01ab686ee2616198ad6f
areebbinnadeem <m.areebbinnadeem@gmail.com>
2024-11-29 23:14:47 +0500

    Retrieving images from S3 and storing csv files to S3

--- IFV_feature_extraction.py
+++ IFV_feature_extraction.py
@@ -2,51 +2,65 @@ import numpy as np
 import cv2
 from scipy import ndimage
 import matplotlib.pyplot as plt
+import boto3
+from io import BytesIO
+from PIL import Image
 
-# Get image
-image_path = "images/W001.jpg"  
-source_image = cv2.imread(image_path, 0)
-#get image shape
+# AWS S3 configuration
+bucket_name = "textile-inspection"  # Replace with your S3 bucket name
+image_key = "images/W001.jpg"    # Replace with your S3 object key
+
+# Initialize S3 client
+s3 = boto3.client("s3")
+
+# Download image from S3
+response = s3.get_object(Bucket=bucket_name, Key=image_key)
+image_data = response["Body"].read()
+
+# Convert the downloaded image to a NumPy array
+image = Image.open(BytesIO(image_data)).convert("L")  # Convert to grayscale
+source_image = np.array(image)
+
+# Get image shape
 image_height, image_width = source_image.shape
-#sliding window parameters
+
+# Sliding window parameters
 window_size = 160  
 window_stride = 1  
- 
-ifv_step=20
-ifv_width=10
-
+ifv_step = 20
+ifv_width = 10
 
 ifv_dx = []
 ifv_dy = []
 
 for y in range(0, image_height - window_size + 1, window_stride):
     for x in range(0, image_width - window_size + 1, window_stride):
-        window = source_image[y:y+window_size, x:x+window_size]
-        for fr in range(0,int(window_size/2)-ifv_width,ifv_step):
+        window = source_image[y:y + window_size, x:x + window_size]
+        for fr in range(0, int(window_size / 2) - ifv_width, ifv_step):
             print(fr)
-            ifv_filter=np.zeros((window_size,window_size))
-            ifv_filter[fr:window_size-fr,fr:window_size-fr]=1
+            ifv_filter = np.zeros((window_size, window_size))
+            ifv_filter[fr:window_size-fr, fr:window_size-fr] = 1
             
-            ifv_filter[fr+ifv_width:window_size-(fr+ifv_width),fr+ifv_width:window_size-(fr+ifv_width)]=0
-            ifwin=ifv_filter*window
+            ifv_filter[fr+ifv_width:window_size-(fr+ifv_width), fr+ifv_width:window_size-(fr+ifv_width)] = 0
+            ifwin = ifv_filter * window
             
-            ifwin_dir=np.array(ndimage.measurements.center_of_mass(ifwin))
-            dx=ifwin_dir[1]-(window_size/2)
-            dy=ifwin_dir[0]-(window_size/2)
+            ifwin_dir = np.array(ndimage.measurements.center_of_mass(ifwin))
+            dx = ifwin_dir[1] - (window_size / 2)
+            dy = ifwin_dir[0] - (window_size / 2)
             
-            ifv_dx.append(dx/(window_size/2))
-            ifv_dy.append(dy/(window_size/2))
+            ifv_dx.append(dx / (window_size / 2))
+            ifv_dy.append(dy / (window_size / 2))
             
             fig = plt.figure()
-            ax0 = fig.add_subplot(1,3,1)
-            ax0.imshow(window, origin='lower', interpolation='None', cmap='gray',alpha=0.6)
+            ax0 = fig.add_subplot(1, 3, 1)
+            ax0.imshow(window, origin='lower', interpolation='None', cmap='gray', alpha=0.6)
             
-            ax2 = fig.add_subplot(1,3,2)
-            ax2.imshow(ifv_filter, origin='lower', interpolation='None', cmap='gray',alpha=0.6)
+            ax2 = fig.add_subplot(1, 3, 2)
+            ax2.imshow(ifv_filter, origin='lower', interpolation='None', cmap='gray', alpha=0.6)
             
-            ax3 = fig.add_subplot(1,3,3)
-            ax3.imshow(ifwin, origin='lower', interpolation='None', cmap='gray',alpha=0.6)
-            ax3.arrow(((window_size-1)/2), ((window_size-1)/2), dx, dy, head_width=0.05, head_length=0.1, fc='g', ec='k')
+            ax3 = fig.add_subplot(1, 3, 3)
+            ax3.imshow(ifwin, origin='lower', interpolation='None', cmap='gray', alpha=0.6)
+            ax3.arrow(((window_size - 1) / 2), ((window_size - 1) / 2), dx, dy, head_width=0.05, head_length=0.1, fc='g', ec='k')
         break
     break
 plt.show()
--- texture_feature_extraction.py
+++ texture_feature_extraction.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import boto3
 import matplotlib.pyplot as plt
 
 from skimage.feature import graycomatrix, graycoprops
@@ -10,165 +11,139 @@ import time
 import cv2
 import numpy as np
 import csv
-
 from scipy import ndimage
+from io import BytesIO
 
+# AWS S3 configuration
+s3 = boto3.client('s3')
+bucket_name = 'textile-inspection'
+image_prefix = 'images/'
+output_file_name = f'features_ws160_wh80.csv'
 
-win_size=160
-win_hop=80
+# Download an image from S3
+def download_image_from_s3(bucket, key):
+    response = s3.get_object(Bucket=bucket, Key=key)
+    return cv2.imdecode(np.frombuffer(response['Body'].read(), np.uint8), cv2.IMREAD_GRAYSCALE)
 
-# Texture Feature List
-featureFile=open('features_ws'+str(win_size)+'_wh'+str(win_hop)+'.csv', 'w',newline='')
-featureFileWriter = csv.writer(featureFile,delimiter=';') 
-line=["Image","h","w","t"]
+# Upload the CSV file to S3
+def upload_csv_to_s3(bucket, key, file_path):
+    s3.upload_file(file_path, bucket, key)
+    print(f"Uploaded {file_path} to s3://{bucket}/{key}")
 
-for n in ["Cont_","Diss_","Homo_","Ener_","Corr_"]:
-        for angle in [0,45,90]:
-            line.append(n+"_"+str(angle))
-            #line.append(n+"_"+str(angle)+"_ET")
+# Define constants
+win_size = 160
+win_hop = 80
 
-for lbl in ["20","40","80"]:
-    line.append("LBP_"+lbl)
-    #line.append("lbp "+lbl+" ET")
+# Create the CSV file locally before uploading
+local_csv_path = output_file_name
+featureFile = open(local_csv_path, 'w', newline='')
+featureFileWriter = csv.writer(featureFile, delimiter=';')
+line = ["Image", "h", "w", "t"]
 
-for theta in np.arange(0,np.pi,np.pi/8):
-    for sigma in (1, 3 ,5):
-        for frequency in [0.1, 0.3, 0.5, 1, 3, 5 ]:
-            line.append("GFB_M_"+str(theta)+"_"+str(sigma)+"_"+str(frequency))
-            line.append("GFB_V_"+str(theta)+"_"+str(sigma)+"_"+str(frequency))
-            line.append("GFB_PP_"+str(theta)+"_"+str(sigma)+"_"+str(frequency))
+for n in ["Cont_", "Diss_", "Homo_", "Ener_", "Corr_"]:
+    for angle in [0, 45, 90]:
+        line.append(n + "_" + str(angle))
 
-ifv_step=20
-ifv_width=10
-for fr in range(0,int(win_size/2)-ifv_width, ifv_step):            
-    line.append("ifv_dx_"+str(fr)+"_"+str(ifv_width))
-    line.append("ifv_dy_"+str(fr)+"_"+str(ifv_width))
+for lbl in ["20", "40", "80"]:
+    line.append("LBP_" + lbl)
 
-# Write the coulmn names
+for theta in np.arange(0, np.pi, np.pi/8):
+    for sigma in (1, 3, 5):
+        for frequency in [0.1, 0.3, 0.5, 1, 3, 5]:
+            line.append("GFB_M_" + str(theta) + "_" + str(sigma) + "_" + str(frequency))
+            line.append("GFB_V_" + str(theta) + "_" + str(sigma) + "_" + str(frequency))
+            line.append("GFB_PP_" + str(theta) + "_" + str(sigma) + "_" + str(frequency))
+
+ifv_step = 20
+ifv_width = 10
+for fr in range(0, int(win_size/2)-ifv_width, ifv_step):
+    line.append("ifv_dx_" + str(fr) + "_" + str(ifv_width))
+    line.append("ifv_dy_" + str(fr) + "_" + str(ifv_width))
+
+# Write the column names
 featureFileWriter.writerow(line)
-linecount=0
+linecount = 0
 
 # Gabor Filter Bank Feature Extractor
 def gaborfilterBankfeats(image):
-    btime=time.time()
     kernels = []
-    for theta in np.arange(0,np.pi,np.pi/8):
-        for sigma in (1, 3 ,5):
-            for frequency in [0.1, 0.3, 0.5, 1, 3, 5 ]:
-                kernel = np.real(gabor_kernel(frequency, theta=theta* np.pi,sigma_x=sigma, sigma_y=sigma))
+    for theta in np.arange(0, np.pi, np.pi/8):
+        for sigma in (1, 3, 5):
+            for frequency in [0.1, 0.3, 0.5, 1, 3, 5]:
+                kernel = np.real(gabor_kernel(frequency, theta=theta, sigma_x=sigma, sigma_y=sigma))
                 kernels.append(kernel)
     feats = np.zeros((len(kernels), 3), dtype=np.double)
-    
+
     for k, kernel in enumerate(kernels):
         filtered = ndi.convolve(image, kernel, mode='wrap')
         feats[k, 0] = filtered.mean()
         feats[k, 1] = filtered.var()
-        feats[k, 2] = filtered.max()-filtered.min()
-    et=btime-time.time()
-    return [feats,et]
+        feats[k, 2] = filtered.max() - filtered.min()
 
-def IFV(source_image,window_size,ifv_step,ifv_width):
+    return feats
+
+# IFV feature extraction
+def IFV(source_image, window_size, ifv_step, ifv_width):
     image_height, image_width = source_image.shape
     ifv_dx = []
     ifv_dy = []
-    for fr in range(0,int(window_size/2)-ifv_width,ifv_step):
-        ifv_filter=np.zeros((window_size,window_size))
-        ifv_filter[fr:window_size-fr,fr:window_size-fr]=1
-        #IFV filtering
-        ifv_filter[fr+ifv_width:window_size-(fr+ifv_width),fr+ifv_width:window_size-(fr+ifv_width)]=0
-        ifwin=ifv_filter*source_image
-        #IFV calculation
-        ifwin_dir=np.array(ndimage.measurements.center_of_mass(ifwin))
-        dx=ifwin_dir[1]-(window_size/2)
-        dy=ifwin_dir[0]-(window_size/2)
-        #normalization
-        ifv_dx.append(dx/(window_size/2))
-        ifv_dy.append(dy/(window_size/2))
+    for fr in range(0, int(window_size/2)-ifv_width, ifv_step):
+        ifv_filter = np.zeros((window_size, window_size))
+        ifv_filter[fr:window_size-fr, fr:window_size-fr] = 1
+        ifv_filter[fr+ifv_width:window_size-(fr+ifv_width), fr+ifv_width:window_size-(fr+ifv_width)] = 0
+        ifwin = ifv_filter * source_image
+        ifwin_dir = np.array(ndimage.measurements.center_of_mass(ifwin))
+        dx = ifwin_dir[1] - (window_size/2)
+        dy = ifwin_dir[0] - (window_size/2)
+        ifv_dx.append(dx / (window_size/2))
+        ifv_dy.append(dy / (window_size/2))
     return ifv_dx, ifv_dy
 
-## Texture Feature Extraction
-for i in range(1,131):
-    filename="W"+str(i).zfill(3)+".jpg"
-    source_image = cv2.imread("images/"+filename,0)
-    
-    height = source_image.shape[0]
-    width = source_image.shape[1]
-    
-    half_win = win_size // 2
+# Process each image in S3
+for i in range(1, 131):
+    filename = f"W{str(i).zfill(3)}.jpg"
+    s3_key = f"{image_prefix}{filename}"
 
+    source_image = download_image_from_s3(bucket_name, s3_key)
+    height, width = source_image.shape
+
+    half_win = win_size // 2
     for y in range(half_win, height - half_win, win_hop):
         for x in range(half_win, width - half_win, win_hop):
-            linecount=linecount+1
+            linecount += 1
             print("###############################################")
-            print(linecount,"File:"+filename, "x:",x,"y:",y)
-            line=[filename, str(y),str(x),str(i)]
-            
-            sw= (y - half_win)+ (y + half_win + 1)
-            sh= (x - half_win)+ (x + half_win + 1)
-            window = source_image[y - half_win:y + half_win , x - half_win:x + half_win ]
-            
-            """
-            plt.close()
-            plt.subplot(2,2,1)
-        
-            plt.imshow(source_image)
-            ax = plt.gca()
-            rect = patches.Rectangle((x,y),win_size,win_size,linewidth=1,edgecolor='r',facecolor='none')
-            ax.add_patch(rect)
-            plt.subplot(2,2,2)
-            plt.imshow(window)
-        
-            plt.show()
-            plt.waitforbuttonpress()
-            
-            """
-            #GLCM http://matlab.izmiran.ru/help/toolbox/images/enhanc15.html
-            glcm = graycomatrix(window, [1], [0, 0.25*np.pi, 0.5*np.pi], 256, symmetric=True, normed=True)
-            for feature in ["contrast","dissimilarity","homogeneity","energy","correlation"]:
-                btime=time.time()
-                fang=graycoprops(glcm, feature)
-                #print("glcm",feature, fang)
+            print(linecount, "File:", filename, "x:", x, "y:", y)
+            line = [filename, str(y), str(x), str(i)]
+
+            window = source_image[y - half_win:y + half_win, x - half_win:x + half_win]
+
+            # GLCM
+            glcm = graycomatrix(window, [1], [0, 0.25 * np.pi, 0.5 * np.pi], 256, symmetric=True, normed=True)
+            for feature in ["contrast", "dissimilarity", "homogeneity", "energy", "correlation"]:
+                fang = graycoprops(glcm, feature)
                 for fa in fang[0]:
-                    line.append(str(fa).replace(".",","))
-                    et=btime-time.time()
-                    #line.append("et:"+str(et).replace(".",","))
-        
-            #LBP https://liris.cnrs.fr/Documents/Liris-5004.pdf
-            btime=time.time()
-            lbp80 = local_binary_pattern(window, 20, win_size/2, 'uniform')
-            line.append(str(np.sum(lbp80)).replace(".",","))
-            et=btime-time.time()
-            #line.append("et:"+str(et).replace(".",","))
-            
-            btime=time.time()
-            lbp80 = local_binary_pattern(window, 40, win_size/2, 'uniform')
-            line.append(str(np.sum(lbp80)).replace(".",","))
-            et=btime-time.time()
-            #line.append("et:"+str(et).replace(".",","))
-            
-            btime=time.time()
-            lbp160 = local_binary_pattern(window, 80, win_size/2, 'uniform')
-            line.append(str(np.sum(lbp160)).replace(".",","))
-            et=btime-time.time()
-            #line.append("et:"+str(et).replace(".",","))
-            #print("lbp4:",et)
-        
-            # gabor filter bank
-            fb_feat,et=gaborfilterBankfeats(window)
+                    line.append(str(fa).replace(".", ","))
+
+            # LBP
+            lbp = local_binary_pattern(window, 20, win_size/2, 'uniform')
+            line.append(str(np.sum(lbp)).replace(".", ","))
+
+            # Gabor Filter Bank
+            fb_feat = gaborfilterBankfeats(window)
             for gff in fb_feat:
-                line.append(str(gff[0]).replace(".",","))
-                line.append(str(gff[1]).replace(".",","))
-                line.append(str(gff[2]).replace(".",","))
-            #line.append("et:"+str(et/144).replace(".",","))
-            
+                line.append(str(gff[0]).replace(".", ","))
+                line.append(str(gff[1]).replace(".", ","))
+                line.append(str(gff[2]).replace(".", ","))
+
             # IFV
-            ifv_step=20
-            ifv_width=10
-            ifv_dx,ifv_dy=IFV(window, win_size ,ifv_step,ifv_width)
+            ifv_dx, ifv_dy = IFV(window, win_size, ifv_step, ifv_width)
             for dx in ifv_dx:
-                line.append(str(dx).replace(".",","))
+                line.append(str(dx).replace(".", ","))
             for dy in ifv_dy:
-                line.append(str(dx).replace(".",","))
+                line.append(str(dy).replace(".", ","))
+
             featureFileWriter.writerow(line)
+
+# Close CSV file and upload to S3
 featureFile.close()
-           
+upload_csv_to_s3(bucket_name, f"output/{output_file_name}", local_csv_path)
